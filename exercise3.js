c7b262218ed8cbb933f2c1e21ec35e83426f59f3
blueberryenjoyer <phoenix.wheatley@gmail.com>
2023-05-25 14:54:57 -0400

    did ALL the things. excercise 4 went a bit hard

--- exercise3.js
+++ exercise3.js
@@ -1,13 +1,36 @@
 // Something that's important to be aware of is that you can set multiple event listeners on the same element. We'll practice that in this exercise.
 
 // First, we have the #cell-one element. We will give two event listeners to this element:
-    // 1) An event listener that, when the element is clicked, will turn the background color to green.
-    // 2) An event listener that, when the element is double-clicked, will turn the background color to blue. 
+// 1) An event listener that, when the element is clicked, will turn the background color to green.
+// 2) An event listener that, when the element is double-clicked, will turn the background color to blue. 
+let cell1 = document.getElementById("cell-one")
+let cell2 = document.getElementById("cell-two")
+let cell3 = document.getElementById("cell-three")
+function cellone() {
+    cell1.style.backgroundColor = "green"
+}
+function cellone2() {
+    cell1.style.backgroundColor = "blue"
+}
 
 // Next, we'll target the #cell-two element. We will give two event listeners to this element:
-    // 1) An event listener that, when the element is hovered over, will turn the border into a green, 2px width, and solidly-lined border. 
-    // 2) An event listener that, when the user removes their mouse from over the element, will turn the border back into a black, 2 px width, and solidly-lined border.  
+// 1) An event listener that, when the element is hovered over, will turn the border into a green, 2px width, and solidly-lined border.
+// 2) An event listener that, when the user removes their mouse from over the element, will turn the border back into a black, 2 px width, and solidly-lined border.
+function celltwo() {
+    cell2.style.border = "2px solid green"
+}
+function celltwo2() {
+    cell2.style.border = "2px solid black"
+}
+
 
 // Lastly, we'll target the #cell-three element. We will give two event listeners to this element:
-    // 1) An event listener that, when the element is clicked, will fill the element with a text of "Stop clicking me!"
-    // 2) An event listener that, when the element is right-clicked, will fill the element with a text of "No right clicking either!!"
+// 1) An event listener that, when the element is clicked, will fill the element with a text of "Stop clicking me!"
+// 2) An event listener that, when the element is right-clicked, will fill the element with a text of "No right clicking either!!"
+
+function cellthree() {
+    cell3.innerHTML = "dont click on me!"
+}
+function cellthree1() {
+    cell3.innerHTML = "you think youre safe? you think this box confines me? i will find you, i will capture you, and no box will ever be rightclicked again"
+}
--- exercise4.js
+++ exercise4.js
@@ -1,5 +1,13 @@
-// You now have a 3x3 grid. We want to configure this grid such that when the left 3 cells are clicked, the colors turn red, when the middle 3 cells are clicked, the color turns green, and when the last 3 cells are clicked, the color turns blue. 
+// You now have a 3x3 grid. We want to configure this grid such that when the left 3 cells are clicked, the colors turn red, when the middle 3 cells are clicked, the color turns green, and when the last 3 cells are clicked, the color turns blue.
 
 // Write some JS code below such that event listeners are attached to all the correct elements following the specifications above.
 
 // Hint: Some sort of loop may be helpful to you in this process!
+
+function superFuncy(input) {
+    if (input.outerHTML.includes("green")) { input.style.backgroundColor = "green" }
+    else if (input.outerHTML.includes("red")) { input.style.backgroundColor = "red" }
+    else if (input.outerHTML.includes("blue")) { input.style.backgroundColor = "blue" }
+}
+
+//i tried way too hard making this the way i did
